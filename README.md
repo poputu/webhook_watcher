f8c5f8a8d2b4243786376ac7ffbeb2d8bd1345bc
Oleh <oleh@flash-fx.com.au>
2023-03-22 19:24:01 +0200

    add simple http redirect

--- README.md
+++ README.md
@@ -21,15 +21,20 @@ This is a Node.js application that creates a webhook listener and allows you to 
 
 ## Usage
 
-1. Start the server by running the following command:
+1. To redirect requests to another application, you'll need to set the `REDIRECT_PORT` environment variable. 
+This can be done by adding the variable to your system environment variables or by setting it in your IDE's run configuration, 
+depending on your development environment. Once the variable is set, the application will 
+automatically redirect requests to the specified port.
+
+2. Start the server by running the following command:
 
     -Run `node app.js`
 
-2. Open your browser and navigate to `http://localhost:3099`. You should see a message saying "Link, where listener is running:" followed by a link. This is the link where your webhook listener is running.
+3. Open your browser and navigate to `http://localhost:3099`. You should see a message saying "Link, where listener is running:" followed by a link. This is the link where your webhook listener is running.
 
-3. Use provided link as the webhook URL when setting up a webhook on any service.
+4. Use provided link as the webhook URL when setting up a webhook on any service.
 
-4. When a webhook is sent to your listener, the payload will be displayed on the webpage.
+5. When a webhook is sent to your listener, the payload will be displayed on the webpage.
 
 ## License
 
--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const { spawn } = require('child_process');
-const port = 3099
+const bodyParser = require("body-parser");
+const { spawn } = require("child_process");
+const REDIRECT_PORT = process.env.REDIRECT_PORT;
+const port = 3099;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-let hookUrl
+let hookUrl;
 const requests = [];
 
 // Start the cloudflared tunnel
-const tunnel = spawn('cloudflared', ['tunnel', '--url', `http://localhost:${port}`]);
+const tunnel = spawn("cloudflared", [
+  "tunnel",
+  "--url",
+  `http://localhost:${port}`,
+]);
 
-tunnel.stdout.on('data', data => {
+tunnel.stdout.on("data", (data) => {
   console.log(`stdout: ${data}`);
 });
 
-tunnel.stderr.on('data', data => {
+tunnel.stderr.on("data", (data) => {
   const regex = /https:\/\/\S+/g;
-  let match = regex.exec(`${data}`)
+  let match = regex.exec(`${data}`);
   if (match) {
-    hookUrl = match
+    hookUrl = match;
   }
   console.error(`stderr: ${data}`);
 });
 
-tunnel.on('close', code => {
+tunnel.on("close", (code) => {
   console.log(`child process exited with code ${code}`);
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   let body = `<h3>Link, where listener is running:</h3> ${hookUrl} 
-<button onclick="copyHookUrl()" style="marging: 5px">Copy</button>
+<button onclick="copyHookUrl()" style="margin: 5px">Copy</button>
 <h2>List of Previous Requests:</h2><ul>`;
   requests.forEach((request, index) => {
     body += `<li>Request ${index + 1}: ${JSON.stringify(request)}</li>`;
   });
-  body += '</ul>';
+  body += "</ul>";
 
   // Add a meta refresh tag to reload the page every 5 seconds
   body += '<meta http-equiv="refresh" content="5">';
@@ -59,13 +64,33 @@ app.get('/', (req, res) => {
   res.send(body);
 });
 
-
-app.post('/', (req, res) => {
-  console.log("New Request: ", req.body)
+app.post("/", (req, res) => {
+  console.log("New Request: ", req.body);
   requests.push(req.body);
-  res.send('Request received');
+
+  // Send the request to the other application
+  if (REDIRECT_PORT) {
+    const http = require("http");
+    const request = http.request(
+      {
+        hostname: "localhost",
+        port: REDIRECT_PORT,
+        path: "/",
+        method: "POST",
+        headers: req.headers,
+      },
+      (response) => {
+        console.log("Response from redirect server:", response.statusCode);
+      }
+    );
+
+    request.write(JSON.stringify(req.body));
+    request.end();
+  }
+
+  res.send("Request received");
 });
 
 app.listen(port, () => {
-  console.log('Server is running on port 3099');
+  console.log(`Server is running on port: ${port}`);
 });
